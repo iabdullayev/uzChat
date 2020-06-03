fd8def284be76eab1a1e1c8fdd15bd559b6451f1
Iskandar Abdullayev <iskandar.abdullayev@gmail.com>
2020-06-04 02:06:04 +0500

    Added SDWebImage Pod

---
+++ UzChat/Services/ListenerService.swift
@@ -0,0 +1,53 @@
+//
+//  ListenerService.swift
+//  UzChat
+//
+//  Created by Feruza Atahodjaeva on 6/4/20.
+//  Copyright © 2020 Home. All rights reserved.
+//
+
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+
+class ListenerService {
+    
+    static let shared = ListenerService()
+    
+    private let db = Firestore.firestore()
+    
+    private var usersRef: CollectionReference {
+        return db.collection("users")
+    }
+    
+    private var currentUserId: String {
+        return Auth.auth().currentUser!.uid
+    }
+    
+    func usersObserve(users: [MUser], completion: @escaping (Result<[MUser], Error>) -> Void) -> ListenerRegistration? {
+        var users = users
+        let usersListener = usersRef.addSnapshotListener { (querySnapshot, error) in
+            guard let snapshot = querySnapshot else {
+                completion(.failure(error!))
+                return
+            }
+            snapshot.documentChanges.forEach { (diff) in
+                guard let muser = MUser(document: diff.document) else { return }
+                switch diff.type {
+                    
+                case .added:
+                    guard !users.contains(muser) else { return }
+                    guard muser.id != self.currentUserId else { return }
+                case .modified:
+                    guard let index = users.firstIndex(of: muser) else { return }
+                    users[index] = muser
+                case .removed:
+                    guard let index = users.firstIndex(of: muser) else { return }
+                    users.remove(at: index)
+                }
+            }
+            completion(.success(users))
+        }
+        return usersListener
+    }
+}
